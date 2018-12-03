a25ffbd51c33ecf7edbfe161775711d8b4a11508
Rostyslav Toch <RokaRostuk@gmail.com>
2018-12-03 19:22:36 +0200

    Implemented titles

---
+++ src/components/MainSlider/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './index.css'
+
+const MainSlider = () =>
+    <div className="main-slider">
+        <div className="big-title">
+            Quick Search for Your <span style={{ color: "#0E7200" }}>Perfect Product</span>
+        </div>
+        <div className="small-title">
+            INNOVATE • LIST • CONNECT
+        </div>
+    </div>
+
+export default MainSlider;
--- src/components/TopSection/index.tsx
+++ src/components/TopSection/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import './index.css';
 import Header from '../Header';
+import MainSlider from '../MainSlider';
 
 const TopSection = () =>
     <div className="top-section">
         <Header />
+        <MainSlider />
     </div>
 
 export default TopSection;
