cf3509c2d7faa8131f4c7b78a0dd5f2471a13b72
Rostyslav Toch <RokaRostuk@gmail.com>
2018-12-06 16:15:36 +0200

    Added header transition

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,36 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import './index.scss';
+import TrackVisibility from 'react-on-screen';
 
-const Header = () =>
-    <div className="main-header">
-        <div className="main-logo" />
-        <div className="navigation-bar">
-            <Link to={'/'} className="link">ALL PRODUCTS</Link>
-            <Link to={'/'} className="link">ABOUT US</Link>
-            <Link to={'/'} className="link">LOG IN</Link>
-            <div className="shape" />
-            <Link to={'/'} className="link">SIGN UP</Link>
+export interface Props {
+    isVisible?: boolean;
+}
+
+const Header = ({ isVisible } : Props) => {
+    const style = !isVisible ? {
+        boxShadow: '0 0 6px rgba(0,0,2,0.16)',
+        background: 'rgba(235,236,240)',
+        paddingTop: '0px',
+        height: '80px'
+    } : {};
+    return (
+        <div className="main-header" style={style}>
+            <Link to={'/'} className="main-logo" />
+            <div className="navigation-bar">
+                <Link to={'/'} className="link">ALL PRODUCTS</Link>
+                <Link to={'/'} className="link">ABOUT US</Link>
+                <Link to={'/'} className="link">LOG IN</Link>
+                <div className="shape" />
+                <Link to={'/'} className="link">SIGN UP</Link>
+            </div>
         </div>
-    </div>
+    );
+}
 
-export default Header;
+const TrackedHeader = () =>
+    <TrackVisibility partialVisibility>
+        <Header />
+    </TrackVisibility>
+
+export default TrackedHeader;
